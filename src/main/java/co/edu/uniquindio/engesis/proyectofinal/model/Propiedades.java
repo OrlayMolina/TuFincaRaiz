7ba82abf5190c1129eda762f7ad41ecd9eed9339
Guarin0829 <daniela.lopezg1@uqvirtual.edu.co>
2022-11-15 23:52:19 -0500

    Propiedades
    Se realiza interfaz grafica de propiedades

---
+++ src/main/java/co/edu/uniquindio/engesis/proyectofinal/model/Propiedades.java
@@ -0,0 +1,136 @@
+package co.edu.uniquindio.engesis.proyectofinal.model;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Propiedades {
+
+    @FXML
+    private Button btnAcuatlizar;
+
+    @FXML
+    private Button btnBuscar;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private Button btnGuardarUsuario;
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML
+    private ComboBox<?> cbTipoDocumento;
+
+    @FXML
+    private ComboBox<?> cbTipoUsuario;
+
+    @FXML
+    private Label lblHeaderName;
+
+    @FXML
+    private Label lblPrincipal;
+
+    @FXML
+    private TableColumn<?, ?> tblConsecutivo;
+
+    @FXML
+    private TableColumn<?, ?> tblCorreo;
+
+    @FXML
+    private TableColumn<?, ?> tblDocumento;
+
+    @FXML
+    private TableColumn<?, ?> tblPrimerApellido;
+
+    @FXML
+    private TableColumn<?, ?> tblPrimerNombre;
+
+    @FXML
+    private TableColumn<?, ?> tblSegundoApellido;
+
+    @FXML
+    private TableColumn<?, ?> tblSegundoNombre;
+
+    @FXML
+    private TableColumn<?, ?> tblTipoDocumento;
+
+    @FXML
+    private PasswordField txtContrasenia;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private TextField txtNombreUsuario;
+
+    @FXML
+    private TextField txtNumeroDocumento;
+
+    @FXML
+    private TextField txtPrimerApellido;
+
+    @FXML
+    private TextField txtPrimerNombre;
+
+    @FXML
+    private TextField txtSegundoApellido;
+
+    @FXML
+    private TextField txtSegundoNombre;
+
+    @FXML
+    private TextField txtTelefono;
+
+    @FXML
+    void onActualizar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBuscar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCancelarRegistro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onComboBox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onComboBoxTipoUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEliminar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSalirClic(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onguardar(ActionEvent event) {
+
+    }
+
+}
