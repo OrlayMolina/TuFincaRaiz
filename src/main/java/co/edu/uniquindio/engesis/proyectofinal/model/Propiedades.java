16cb6eac529ed1771569ce36e278724147c29f7b
Guarin0829 <daniela.lopezg1@uqvirtual.edu.co>
2022-11-18 23:39:21 -0500

    Se agregan botones a la clase Propiedades

--- src/main/java/co/edu/uniquindio/engesis/proyectofinal/model/Propiedades.java
+++ src/main/java/co/edu/uniquindio/engesis/proyectofinal/model/Propiedades.java
@@ -46,37 +46,106 @@ public class Propiedades {
     private Label lblPrincipal;
 
     @FXML
-    void onApartamentoClick(ActionEvent event) {
+    void onApartamentoClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("PropiedadesApartamento.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Tu Finca Raíz |  PropiedadesApartamento");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.show();
 
     }
 
     @FXML
-    void onBodegaClick(ActionEvent event) {
+    void onBodegaClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("PropiedadesBodega.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Tu Finca Raíz |  PropiedadesBodega");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.show();
 
     }
 
     @FXML
-    void onCasaClick(ActionEvent event) {
+    void onCasaClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("PropiedadesCasa.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Tu Finca Raíz |  PropiedadesCasa");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    @FXML
+    void onChaletClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("PropiedadesChalet.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Tu Finca Raíz |  PropiedadesChalet");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.show();
 
     }
 
     @FXML
-    void onChaletClick(ActionEvent event) {
+    void onEdificioClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("PropiedadesEdificio.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Tu Finca Raíz |  PropiedadesEdificio");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.show();
 
     }
 
     @FXML
-    void onEdificioClick(ActionEvent event) {
+    void onLoteClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("PropiedadesLote.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Tu Finca Raíz |  PropiedadesLote");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.show();
 
     }
 
     @FXML
-    void onLoteClick(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onParqueaderoClick(ActionEvent event) {
+    void onParqueaderoClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("PropiedadesParqueadero.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Tu Finca Raíz |  PropiedadesParqueadero");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.show();
 
     }
 
@@ -95,6 +164,7 @@ public class Propiedades {
         //Prueba
 
     }
+
 
 
 }
