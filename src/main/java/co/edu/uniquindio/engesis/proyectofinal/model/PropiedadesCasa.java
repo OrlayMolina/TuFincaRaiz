cc0f1a31dddfe163b904de8a96375b57ab786300
Guarin0829 <daniela.lopezg1@uqvirtual.edu.co>
2022-11-18 23:46:36 -0500

    se realizan cambios

--- src/main/java/co/edu/uniquindio/engesis/proyectofinal/model/PropiedadesCasa.java
+++ src/main/java/co/edu/uniquindio/engesis/proyectofinal/model/PropiedadesCasa.java
@@ -2,6 +2,10 @@ package co.edu.uniquindio.engesis.proyectofinal.model;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -19,6 +23,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PropiedadesCasa {
 
@@ -123,7 +130,17 @@ public class PropiedadesCasa {
     }
 
     @FXML
-    void onSalirClic(ActionEvent event) {
+    void onSalirCasaClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("Propiedades.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Tu Finca Raíz | Propiedades");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.show();
 
     }
 
